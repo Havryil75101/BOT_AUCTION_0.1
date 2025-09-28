66743d3f42da9ac688976589985c49ad49750042
Havryil <160042162+Havryil75101@users.noreply.github.com>
2025-09-28 08:29:29 +0200

    logic.py

--- logic.py
+++ logic.py
@@ -92,7 +92,27 @@ class DatabaseManager:
             cur.execute('SELECT * FROM prizes wHERE used = 0 ORDER BY RANDOM() LIMIT 1')
         return cur.fetchall()[0]
     
-  
+    def get_winners_count(self, prize_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('SELECT - COUNT(*) FROM winners WHE>RE prize_id = ?', (prize_id, ))
+            return cur.fetchall()[0][0]
+   
+    def get_rating(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('''
+            SELECT users.user_name, COUNT(winners.prize_id) as count_prize
+            FROM winners
+            INNER JOIN users ON users.user_id = winners.user_id
+            GROUP BY winners.user_id
+            ORDER BY count_prize DESC
+            LIMIT 10
+            ''')
+            return cur.fetchall()
+    
 def hide_img(img_name):
     image = cv2.imread(f'img/{img_name}')
     blurred_image = cv2.GaussianBlur(image, (15, 15), 0)
